abcc1391208b8aed7bf8da7e706ef60eb0a5e617
naveena-0 <manjulamanjuk07@gmail.com>
2026-06-20 21:12:38 +0530

    src

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <h6 className="text-sm text-center w-full">
+      Created via 💙{" "}
+      <a
+        href="https://github.com/naveena-0"
+        target="_blank"
+        alt="Naveena's GitHub Account"
+        className="hover:underline"
+      >
+        NAVEENA K
+      </a>
+    </h6>
+  );
+}
+
+export default Footer;
